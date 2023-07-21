a22c40e76403d81b305cf24f98c96fde5372f128
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-07-21 02:18:52 +0000

    Update A1.sh

--- A1.sh
+++ A1.sh
@@ -1,5 +1,6 @@
-xdg-open https://instagram.com/mike90s15
-sleep 2
-xdg-open https://t.me/Clownters 
-sleep 2
+#!/usr/bin/env bash
+xdg-open https://instagram.com/mike90s15 &>/dev/null && sleep 30
+xdg-open https://twitter.com/mike90s15 &>/dev/null && sleep 30
+xdg-open https://www.tiktok.com/@mike90s15 &>/dev/null && sleep 30
+xdg-open https://t.me/channel_90s15 &>/dev/null && sleep 30
 gcc main.c -o main &&./main || printf "\c\e[1;31m Erro\n"
